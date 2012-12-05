7f09867ad71b347948ac35ee6afcbbccbe9b7b8c
Cody Scott <cs475x@gmail.com>
2012-12-04 21:01:51 -0800

    Consistency and cleanup

--- class.firmwarelinks-api.php
+++ class.firmwarelinks-api.php
@@ -1,49 +1,51 @@
 <?php
-/**
- * Firmware Links API for PHP
- * (C) 2012 Callum Jones <cj@icj.me>
- */
 
-class FirmwareLinks
-{
-	private $user_agent = "firmwarelinks-api <cj@icj.me>";
-	
-	public function getData($request, $device, $buildid) {
-		// check input
-		if (empty($device)) {
-			throw new Exception("No device set.");
-		}		
-		if (empty($request)) {
-			throw new Exception("No data requested.");
-		}
-		if (empty($buildid)) {
-			throw new Exception("No buildid set.");
-		}
-		$ch = curl_init();
-		$timeout = 5;
-		
-		// set the URL
-		$url = "http://api.ios.icj.me/v2/$device/$buildid/$request";
-		
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-		curl_setopt($ch, CURLOPT_USERAGENT, $this->user_agent);
-		
-		$returned_data = curl_exec($ch);
-		curl_close($ch);
-		
-		// check if the link is returned properly
-		
-		$response = get_headers($url, 1);
-				
-		if($response[0] == "HTTP/1.1 200 OK") {
-			return $returned_data;
-		} else {
-			throw new Exception("Error: " . $response[0]);
+	/**
+	* Firmware Links API for PHP
+	* (C) 2012 Callum Jones <cj@icj.me>
+	*/
+	class FirmwareLinks {
+
+		public function getData($request, $device, $buildid) {
+
+			// Validate user input
+			$r = new ReflectionMethod(__CLASS__, __FUNCTION__);
+			$args = array();
+			foreach ($r->getParameters() as $param) {
+				$name = $param->getName();
+				if (empty(${$name}))
+					throw new Exception("The `$name` parameter cannot be empty.");
+				elseif (!is_string(${$name}))
+					throw new Exception("The `$name` parameter must be a string.");
+			}
+
+			// Set the request URL
+			$url = "http://api.ios.icj.me/v2/$device/$buildid/$request";
+
+			// Initialize and configure cURL
+			$ch = curl_init();
+			$options = array(
+				CURLOPT_URL => $url,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_CONNECTTIMEOUT => 5,
+				CURLOPT_USERAGENT => "firmwarelinks-api <cj@icj.me>"
+			);
+			curl_setopt_array($ch, $options);
+
+			// Execute and terminate the cURL request
+			$returned_data = curl_exec($ch);
+			curl_close($ch);
+
+			// Validate the cURL response
+			$response = get_headers($url, 1);
+			if($response[0] == "HTTP/1.1 200 OK")
+				return $returned_data;
+			else
+				throw new Exception("Error: " . $response[0]);
+
 		}
 
 	}
-}
+
 ?>
