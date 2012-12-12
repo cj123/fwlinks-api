dcad6e73a0e6efdd60dd871b47cb4812a0751620
Callum Jones <cj@icj.me>
2012-12-12 16:12:37 +0000

    remove pointless tabs from beginning of each line

--- class.firmwarelinks-api.php
+++ class.firmwarelinks-api.php
@@ -1,50 +1,50 @@
 <?php
+/**
+ * Firmware Links API for PHP Example
+ * (C) 2012 Callum Jones <cj@icj.me>
+ */
 
-	/**
-	 * Firmware Links API for PHP Example
-	 * (C) 2012 Callum Jones <cj@icj.me>
-	 */
-	class FirmwareLinks {
+class FirmwareLinks {
 
-		public function getData($request, $device, $buildid) {
+	public function getData($request, $device, $buildid) {
 
-			// Validate user input
-			$r = new ReflectionMethod(__CLASS__, __FUNCTION__);
-			foreach ($r->getParameters() as $param) {
-				$name = $param->getName();
-				if (empty(${$name}))
-					throw new Exception("The `$name` parameter cannot be empty.");
-				elseif (!is_string(${$name}))
-					throw new Exception("The `$name` parameter must be a string.");
-			}
-
-			// Set the request URL
-			$url = "http://api.ios.icj.me/v2/$device/$buildid/$request";
-
-			// Initialize and configure cURL
-			$ch = curl_init();
-			$options = array(
-				CURLOPT_URL => $url,
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_FOLLOWLOCATION => true,
-				CURLOPT_CONNECTTIMEOUT => 5,
-				CURLOPT_USERAGENT => "firmwarelinks-api <cj@icj.me>"
-			);
-			curl_setopt_array($ch, $options);
-
-			// Execute and terminate the cURL request
-			$returned_data = curl_exec($ch);
-			curl_close($ch);
-
-			// Validate the cURL response
-			$response = get_headers($url, 1);
-			if($response[0] == "HTTP/1.1 200 OK")
-				return $returned_data;
-			else
-				throw new Exception("Error: " . $response[0]);
-
+		// Validate user input
+		$r = new ReflectionMethod(__CLASS__, __FUNCTION__);
+		foreach ($r->getParameters() as $param) {
+			$name = $param->getName();
+			if (empty(${$name}))
+				throw new Exception("The `$name` parameter cannot be empty.");
+			elseif (!is_string(${$name}))
+				throw new Exception("The `$name` parameter must be a string.");
 		}
 
+		// Set the request URL
+		$url = "http://api.ios.icj.me/v2/$device/$buildid/$request";
+
+		// Initialize and configure cURL
+		$ch = curl_init();
+		$options = array(
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_CONNECTTIMEOUT => 5,
+			CURLOPT_USERAGENT => "firmwarelinks-api <cj@icj.me>"
+		);
+		curl_setopt_array($ch, $options);
+
+		// Execute and terminate the cURL request
+		$returned_data = curl_exec($ch);
+		curl_close($ch);
+
+		// Validate the cURL response
+		$response = get_headers($url, 1);
+		if($response[0] == "HTTP/1.1 200 OK")
+			return $returned_data;
+		else
+			throw new Exception("Error: " . $response[0]);
+
 	}
+
+}
 
 ?>
--- example.php
+++ example.php
@@ -1,17 +1,20 @@
 <?php
 
-	/**
-	 * Firmware Links API for PHP Example
-	 * (C) 2012 Callum Jones <cj@icj.me>
-	 */
-	require "class.firmwarelinks-api.php";
+/**
+ * Firmware Links API for PHP Example
+ * (C) 2012 Callum Jones <cj@icj.me>
+ */
 
-	try {
-		$fw = new FirmwareLinks();
-		$download_url = $fw->getData("url", "iPhone4,1", "5.1");
-		echo $download_url;
-	} catch (Exception $e) {
-		echo "Caught exception: " .  $e->getMessage() . "\n";
-	}
+require "class.firmwarelinks-api.php";
+
+try {
+
+	$fw = new FirmwareLinks();
+	$download_url = $fw->getData("url", "iPhone4,1", "5.1");
+	echo $download_url;
+
+} catch (Exception $e) {
+	echo "Caught exception: " .  $e->getMessage() . "\n";
+}
 
 ?>
